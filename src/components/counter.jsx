26ddc636ad0b392158f3ec86b53dddf5c7711066
Johnson Tinuoye <johnsontinuoye@gmail.com>
2019-06-27 09:52:26 +0100

    Event handling - onClick

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,34 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: [
+      { id: "tag1", name: "tag1" },
+      { id: "tag2", name: "tag2" },
+      { id: "tag3", name: "tag3" }
+    ]
   };
+  handleButtonClick() {
+    console.log("You clicked a tag");
+  }
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag.id}>
+            {tag.name}
+            <button onClick={this.handleButtonClick}>Click</button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     return (
       <React.Fragment>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length == 0 && "Please create a new tag."}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
