38ddf63835be2e5dee089ad4b05a32bdb83e99c9
Johnson Tinuoye <johnsontinuoye@gmail.com>
2019-06-27 09:18:43 +0100

    Adding bootstrap classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,23 +2,36 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1,
+    imageSrc: "https://picsum.photos/200",
+    stateClass: "primary"
+  };
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold"
   };
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
+        <img src={this.state.imageSrc} />
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
         <button>Increment</button>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    const { count } = this.state;
+    let classes = "badge m-2 badge-";
+    classes += count === 0 ? "warning" : this.state.stateClass;
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count !== 0 ? (
-      count
-    ) : (
-      <span className="text-white bg-success p-2">Zero</span>
-    );
+    this.state.stateClass = "badge-warning";
+    return count !== 0 ? count : "Zero";
   }
 }
 
